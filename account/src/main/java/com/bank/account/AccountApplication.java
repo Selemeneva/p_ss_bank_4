28721f32428f9d643d0193ace1567d9b4c86e069
Konstantin <kostya.kormashov@gmail.com>
2023-04-14 20:51:52 +0300

    Добавил аннотацию @EnableEurekaClient

--- account/src/main/java/com/bank/account/AccountApplication.java
+++ account/src/main/java/com/bank/account/AccountApplication.java
@@ -2,10 +2,12 @@ package com.bank.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@EnableWebMvc
 @SpringBootApplication
+@EnableWebMvc
+@EnableEurekaClient
 public class AccountApplication {
     public static void main(String[] args) {
         SpringApplication.run(AccountApplication.class, args);
