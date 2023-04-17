2d427d98eb702da2d3804a14395bc1d178e7a0ed
Wladimir-Yakubowskiy <iloveemperor@mail.ru>
2023-04-17 11:20:00 +0300

    написаны комментарии к Init

--- profile/src/main/java/com/bank/profile/util/Init.java
+++ profile/src/main/java/com/bank/profile/util/Init.java
@@ -19,7 +19,9 @@ public class Init {
     private final AuditService auditService;
     private final ProfileMapper profileMapper;
 
-
+/**
+ * Init при запуске модуля создаёт для примера экземпляры сущностей и сохраняет их в базу данных
+ * */
     public Init(RegistrationService registrationService, PassportService passportService, ActualRegistrationService actualRegistrationService, ProfileService profileService, AccountDetailsIdService accountDetailsIdService, AuditService auditService, ProfileMapper profileMapper) {
         this.registrationService = registrationService;
         this.passportService = passportService;
@@ -66,25 +68,37 @@ public class Init {
 
         ActualRegistration actualRegistration = new ActualRegistration();
         actualRegistration.setCountry("Russia");
+        actualRegistration.setRegion("Europa");
+        actualRegistration.setCity("Moscow");
+        actualRegistration.setDistrict("Ololo");
+        actualRegistration.setLocality("Ololo");
+        actualRegistration.setStreet("Street");
+        actualRegistration.setHouseNumber(123);
+        actualRegistration.setHouseBlock(5);
+        actualRegistration.setFlatNumber(123);
         actualRegistration.setIndex(12345L);
         actualRegistrationService.save(actualRegistration);
 
         Profile profile = new Profile();
         profile.setPhoneNumber(1243455L);
+        profile.setEmail("iseeheresy@gmail.com");
+        profile.setNameOnCard("Oleg");
+        profile.setInn(1000L);
+        profile.setSnils(1000L);
         profile.setPassport(passport);
-//        profile.setActualRegistration(actualRegistration);
+        profile.setActualRegistration(actualRegistration);
         profileService.save(profile);
 
-//        AccountDetailsId accountDetailsId1 = new AccountDetailsId();
-//        accountDetailsId1.setOwner(profile);
-//        accountDetailsId1.setAccountId(123456L);
-//        accountDetailsId1.setOwner(profile);
-//        accountDetailsIdService.save(accountDetailsId1);
-//
-//        AccountDetailsId accountDetailsId2 = new AccountDetailsId();
-//        accountDetailsId2.setOwner(profile);
-//        accountDetailsId2.setAccountId(123456789L);
-//        accountDetailsIdService.save(accountDetailsId2);
+        AccountDetailsId accountDetailsId1 = new AccountDetailsId();
+        accountDetailsId1.setOwner(profile);
+        accountDetailsId1.setAccountId(123456L);
+        accountDetailsId1.setOwner(profile);
+        accountDetailsIdService.save(accountDetailsId1);
+
+        AccountDetailsId accountDetailsId2 = new AccountDetailsId();
+        accountDetailsId2.setOwner(profile);
+        accountDetailsId2.setAccountId(123456789L);
+        accountDetailsIdService.save(accountDetailsId2);
 
     }
 }
